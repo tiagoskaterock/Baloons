1d08d01391308f2e6d1d6abf89ffe23c979c97d7
tiagoskaterock <tiagolemespalhano@gmail.com>
2020-03-25 14:19:47 -0300

    Added lines from 29 untill the end and function createBallons() at jogo.js

--- jogo.js
+++ jogo.js
@@ -26,4 +26,20 @@ function startGame(){
 	// inserting seconds in span
 	document.getElementById("chronometer").innerHTML = seconds;
 
+	// ballons quantity
+	var balloonQtd = 80;	
+
+	createBalloons(balloonQtd);
+
+}
+
+function createBalloons(qtd){
+	// alert("createBalloons()");
+	for (var i = 0; i < qtd; i++) {
+		// alert("inside the for loop");
+		var balloon = document.createElement("img");
+		balloon.src = "imagens/balao_azul_pequeno.png";
+		balloon.style.margin = "10px";
+		document.getElementById("cenario").appendChild(balloon);
+	}
 }
